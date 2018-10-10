90909a9ec93242e4d7802ed60125836fbc5949ea
Jonathan Chan <jonchan816@gmail.com>
2018-10-10 08:08:11 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,8 @@
   for (let i = 0; i < 25; i++) {
-    console.log
+    if (i === 1) {
+      console.log("I am 1 strange loop.");
+    } else {
+      console.log(`I am ${i} strange loops.`)
+    }
+    return i;
   }
